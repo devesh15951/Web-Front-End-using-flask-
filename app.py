2569e68cae6a3ec3a9ad787f93eec0092a2ed7aa
devesh15951 <devesh15951@gmail.com>
2018-03-11 16:55:23 -0400

    adding files

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for
+from data import Articles
+app = Flask(__name__, static_folder='images', static_url_path="/images")
+
+Articles = Articles()
+
+
+@app.route('/')
+def index():
+    return render_template('ind_hom.html')
+
+
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')
+
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles=Articles)
+
+
+@app.route('/article/<string:id>')
+def article_id(id):
+    return render_template('article.html', id=id)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,27 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem',
+            'author': 'Devesh Waingankar',
+            'create_date': '02/22/2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem',
+            'author': 'Leo Messi',
+            'create_date': '02/22/2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem',
+            'author': 'Dr Genius',
+            'create_date': '02/22/2018'
+        }
+
+
+    ]
+    return articles
