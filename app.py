652960962f1552760ea1c786298967bbc0c560a1
devesh15951 <devesh15951@gmail.com>
2018-03-29 13:18:01 -0400

    front end

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for
 from data import Articles
-app = Flask(__name__, static_folder='images', static_url_path="/images")
+app = Flask(__name__, static_folder='static', static_url_path="/static")
 
 Articles = Articles()
 
