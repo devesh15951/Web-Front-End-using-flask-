a62a1cc2d49d8e017feaa1a24a8676ab85c88de0
devesh15951 <devesh15951@gmail.com>
2018-03-30 12:44:44 -0400

    'committing'

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, jsonify, Response
+import json
+from wtforms import TextField, Form
 from data import Articles
 app = Flask(__name__, static_folder='static', static_url_path="/static")
 
 Articles = Articles()
 
+cities = ["Bratislava", "brajs", "bramvc",
+          "Bansk Bystrica",
+          "Preov",
+          "Povak Bystrica",
+          "ilina",
+          "Koice",
+          "Ruomberok",
+          "Zvolen",
+          "Poprad"]
 
-@app.route('/')
+
+class SearchForm(Form):
+    autocomp = TextField(id='city_autocomplete')
+
+
+@app.route('/_autocomplete', methods=['GET'])
+def autocomplete():
+    return Response(json.dumps(cities), mimetype='application/json')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('ind_hom.html')
+    form = SearchForm(request.form)
+    return render_template('ind_hom.html', form=form)
 
 
 @app.route('/about', methods=['GET', 'POST'])
